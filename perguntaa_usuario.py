a39a99c60e7f607bf709b8894b1d693904418c83
yanpe <leonardofonsecadmc@gmail.com>
2025-11-16 00:05:04 -0300

    Add files via upload

---
+++ perguntaa_usuario.py
@@ -0,0 +1,16 @@
+
+
+
+a = float(input("Digite um numero: "))
+b = float(input("Digite outro numero: "))
+
+c = (input("Voce quer somar , subtrair ou descobrir qual é maior?"))
+
+soma = a+b
+subtrair = a-b
+maior = a>b or a<b
+
+if  c == "maior":
+    print(f"{a} Maior que {b}")
+     
+
